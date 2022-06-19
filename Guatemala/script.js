7dc8429738eef7b182ba5158741c814737026eb5
Jose Daniel Az <josedanielxela@gmail.com>
2022-06-18 19:49:40 -0600

    Desarrollo: Creación de estructura básica html
    Creación de algunos archivos para el mapa interactivo de Guatemala
    Falta funcionamiento

---
+++ Guatemala/script.js
@@ -0,0 +1,12 @@
+function prueba(id){
+    if(id === "GTM1942"){
+        console.log("Baja Verapaz")
+    }
+    if(id === "GTM1945"){
+        console.log("Quetzaltenango")
+    }
+}
+
+function cambiarInformacion(id){
+    null
+}
