e48d20bfec28cc6f83378d5bb63a92c296c21c81
Jose Daniel Chocoy <danielchocoycordon@hotmail.es>
2022-06-21 16:00:44 -0600

    Revert "Feature/contacts"

--- Guatemala/script.js
+++
@@ -1,9 +0,0 @@
-function changeColor(newTxt,Name) {
-    var fronTxt = document.getElementById('txtf');
-    var Txtp = document.getElementById('txtp');
-    var backTitl=document.getElementById('BCKTL')
-    fronTxt.innerHTML=Name
-    backTitl.innerHTML=Name
-    Txtp.innerHTML=newTxt
-    
- }
