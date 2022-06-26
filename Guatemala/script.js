3690ffec2e5f2d434f86eb75a3c623010c24a871
Jose Daniel Chocoy <danielchocoycordon@hotmail.es>
2022-06-26 11:24:45 -0600

    hot fix: menu hamburguesa

--- Guatemala/script.js
+++ Guatemala/script.js
@@ -1,3 +1,15 @@
+addEventListener("DOMContentLoaded",()=>{
+
+    const btn_menu=document.querySelector(".menu-btn")
+    if(btn_menu){
+        btn_menu.addEventListener("click",()=>{
+            const menu_items=document.querySelector(".menu-items")
+            menu_items.classList.toggle("display")
+        })
+    }
+
+})
+
 function changeTxt(idDpt,Name) {
     var Txtp = document.getElementById('txtp');
     var backTitl=document.getElementById('BCKTL');
